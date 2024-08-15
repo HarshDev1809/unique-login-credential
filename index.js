9b1866433cec6953cbc5ffbdf537a89767bbf480
Harsh1809Dev <dev182000@gmail.com>
2024-08-16 00:47:20 +0530

    new commit: cleaned code

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// const defaultChoice = [
-//   "capitalAlphabet",
-//   "smallAlphabet",
-//   "smallAlphabet",
-//   "smallAlphabet",
-// ];
 const smallAlphabets = "abcdefghijklmnopqrstuvwxyz".split("");
 const capitalAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numbers = "1234567890".split("");
@@ -15,16 +9,7 @@ const refrenceObj = {
   specialCharacter: specialCharacters,
 };
 
-// getArray();
-
-// function getArray() {
-//   for (let i = 0; i <= 25; i++) {
-//     if (i <= 9) numbers.push(i);
-//     smallAlphabets.push(String.fromCharCode(97 + i));
-//     capitalAlphabets.push(String.fromCharCode(65 + i));
-//   }
-// }
-
+// returns array of "element" of length "count"
 const getFilledArray = (element, count) => {
   if (count < 1) {
     throw new Error("Invalid element count: element count is less than 1");
@@ -34,146 +19,150 @@ const getFilledArray = (element, count) => {
   return arr;
 };
 
-// const generatePassword = (args) => {
-//   console.log(args);
-//   const length = 10;
-//   if (length < 4) {
-//     throw new Error(
-//       "Invalid password length: password must be aleast 4 character long"
-//     );
-//   }
-//   let choice = new Array();
-//   if (length < 8) {
-//     choice.push("capitalAlphabet");
-//     choice.push(...getFilledArray("smallAlphabet", length - 3));
-//     choice.push("number");
-//     choice.push("specialCharacter");
-//   } else {
-//     choice.push("capitalAlphabet");
-//     choice.push(...getFilledArray("smallAlphabet", length - 5));
-//     choice.push(...getFilledArray("number", 2));
-//     choice.push(...getFilledArray("specialCharacter", 2));
-//   }
-//   let password = "";
-//   choice.forEach(function (value) {
-//     let ch =
-//       refrenceObj[value][Math.floor(Math.random() * refrenceObj[value].length)];
-//     if (ch !== undefined) password += ch;
-//   });
-//   return password;
-// };
-
-// console.log(generatePassword(9, "hello"));
-
+// default password options
 const defaultPasswordOptions = {
-    length: 8,
-    capitalLetter: 1,
-    smallLetter: 3,
-    number: 2,
-    specialCharacters: 2,
-    random:false
-  };
+  length: 8,
+  capitalLetter: 1,
+  smallLetter: 3,
+  number: 2,
+  specialCharacters: 2,
+  random: false,
+};
 
-  const defaultUsernameOptions = {
-    length: 6,
-    capitalLetter: 1,
-    smallLetter: 3,
-    number: 2,
-    specialCharacters: 0,
-    random:false
+// default username options
+const defaultUsernameOptions = {
+  length: 6,
+  capitalLetter: 1,
+  smallLetter: 3,
+  number: 2,
+  specialCharacters: 0,
+  random: false,
+};
+
+// returns a unique password, with options from user
+export const uniquePassword = (options = {}) => {
+  // Merge user-provided options with defaults
+  const finalOptions = { ...defaultPasswordOptions, ...options };
+  const {
+    length,
+    capitalLetter,
+    smallLetter,
+    number,
+    specialCharacter,
+    random,
+  } = finalOptions;
+
+  // Check if the total requested character count exceeds the password length
+  const totalRequested =
+    capitalLetter + smallLetter + number + specialCharacter;
+  if (totalRequested > length) {
+    throw new Error(
+      "Invalid password length: Total character count exceeds the password length."
+    );
   }
 
-  export const uniquePassword = (options = {}) => {
-    // Merge user-provided options with defaults
-    const finalOptions = { ...defaultPasswordOptions, ...options };
-    console.log(finalOptions)
-    const { length, capitalLetter, smallLetter, number, specialCharacter,random } = finalOptions;
-  
-    // Check if the total requested character count exceeds the password length
-    const totalRequested = capitalLetter + smallLetter + number + specialCharacter;
-    if (totalRequested > length) {
-      throw new Error("Invalid password length: Total character count exceeds the password length.");
-    }
-  
-    let choice = [];
-  
-    // Add the requested number of each character type to the choice array
-    if (capitalLetter > 0) {
-      choice.push(...getFilledArray("capitalAlphabet", capitalLetter));
-    }
-    if (smallLetter > 0) {
-      choice.push(...getFilledArray("smallAlphabet", smallLetter));
-    }
-    if (number > 0) {
-      choice.push(...getFilledArray("number", number));
-    }
-    if (specialCharacter > 0) {
-      choice.push(...getFilledArray("specialCharacter", specialCharacter));
-    }
-  
-    // Fill the remaining length with random choices
-    while (choice.length < length) {
-      const types = ["capitalAlphabet", "smallAlphabet", "number", "specialCharacter"];
-      choice.push(types[Math.floor(Math.random() * types.length)]);
-    }
-  
-    // Shuffle the choice array to make the password less predictable
-    if(random){
-        choice = choice.sort(() => 0.5 - Math.random());
-    }
-    console.log(random)
-    console.log(choice)
-  
-    // Generate the password from the choice array
-    let password = "";
-    choice.forEach(function (value) {
-      const ch = refrenceObj[value][Math.floor(Math.random() * refrenceObj[value].length)];
-      if (ch !== undefined) password += ch;
-    });
-  
-    return password;
-  };
+  let choice = [];
 
-  export const uniqueUsername = (options = {})=>{
-    const finalOptions = { ...defaultUsernameOptions, ...options };
-    const { length, capitalLetter, smallLetter, number, specialCharacter,random } = finalOptions;
-    console.log(finalOptions)
-    const totalRequested = capitalLetter + smallLetter + number + specialCharacter;
-    if (totalRequested > length) {
-      throw new Error("Invalid username length: Total character count exceeds the username length.");
-    }
-    let choice = []
-
-    if (capitalLetter > 0) {
-        choice.push(...getFilledArray("capitalAlphabet", capitalLetter));
-      }
-      if (smallLetter > 0) {
-        choice.push(...getFilledArray("smallAlphabet", smallLetter));
-      }
-      if (number > 0) {
-        choice.push(...getFilledArray("number", number));
-      }
-      if (specialCharacter > 0) {
-        choice.push(...getFilledArray("specialCharacter", specialCharacter));
-      }
-
-      while (choice.length < length) {
-        const types = ["capitalAlphabet", "smallAlphabet", "number", "specialCharacter"];
-        choice.push(types[Math.floor(Math.random() * types.length)]);
-      }
-
-      if(random){
-        choice = choice.sort(() => 0.5 - Math.random());
-    }
-
-    console.log(choice)
-
-    let username = "";
-    choice.forEach(function (value) {
-      const ch = refrenceObj[value][Math.floor(Math.random() * refrenceObj[value].length)];
-      if (ch !== undefined) {username += ch;}
-    });
-  
-    return username;
+  // Add the requested number of each character type to the choice array
+  if (capitalLetter > 0) {
+    choice.push(...getFilledArray("capitalAlphabet", capitalLetter));
   }
-  
+  if (smallLetter > 0) {
+    choice.push(...getFilledArray("smallAlphabet", smallLetter));
+  }
+  if (number > 0) {
+    choice.push(...getFilledArray("number", number));
+  }
+  if (specialCharacter > 0) {
+    choice.push(...getFilledArray("specialCharacter", specialCharacter));
+  }
+
+  // Fill the remaining length with random choices
+  while (choice.length < length) {
+    const types = [
+      "capitalAlphabet",
+      "smallAlphabet",
+      "number",
+      "specialCharacter",
+    ];
+    choice.push(types[Math.floor(Math.random() * types.length)]);
+  }
+
+  // Shuffle the choice array to make the password less predictable
+  if (random) {
+    choice = choice.sort(() => 0.5 - Math.random());
+  }
+  console.log(random);
+  console.log(choice);
+
+  // Generate the password from the choice array
+  let password = "";
+  choice.forEach(function (value) {
+    const ch =
+      refrenceObj[value][Math.floor(Math.random() * refrenceObj[value].length)];
+    if (ch !== undefined) password += ch;
+  });
+
+  return password;
+};
+
+export const uniqueUsername = (options = {}) => {
+  const finalOptions = { ...defaultUsernameOptions, ...options };
+  const {
+    length,
+    capitalLetter,
+    smallLetter,
+    number,
+    specialCharacter,
+    random,
+  } = finalOptions;
+  console.log(finalOptions);
+  const totalRequested =
+    capitalLetter + smallLetter + number + specialCharacter;
+  if (totalRequested > length) {
+    throw new Error(
+      "Invalid username length: Total character count exceeds the username length."
+    );
+  }
+  let choice = [];
+
+  if (capitalLetter > 0) {
+    choice.push(...getFilledArray("capitalAlphabet", capitalLetter));
+  }
+  if (smallLetter > 0) {
+    choice.push(...getFilledArray("smallAlphabet", smallLetter));
+  }
+  if (number > 0) {
+    choice.push(...getFilledArray("number", number));
+  }
+  if (specialCharacter > 0) {
+    choice.push(...getFilledArray("specialCharacter", specialCharacter));
+  }
+
+  while (choice.length < length) {
+    const types = [
+      "capitalAlphabet",
+      "smallAlphabet",
+      "number",
+      "specialCharacter",
+    ];
+    choice.push(types[Math.floor(Math.random() * types.length)]);
+  }
+
+  if (random) {
+    choice = choice.sort(() => 0.5 - Math.random());
+  }
+
+  console.log(choice);
+
+  let username = "";
+  choice.forEach(function (value) {
+    const ch =
+      refrenceObj[value][Math.floor(Math.random() * refrenceObj[value].length)];
+    if (ch !== undefined) {
+      username += ch;
+    }
+  });
+
+  return username;
+};
